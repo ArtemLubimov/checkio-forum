f3078b79049de5cdfa6e9b14276feccec66656c4
ArtemLubimov <Patrik.ms94@gmail.com>
2015-08-16 08:16:19 +0300

    it send and mention user

--- checkio_forum/libs/spirit_email_notification/signals.py
+++ checkio_forum/libs/spirit_email_notification/signals.py
@@ -6,33 +6,60 @@ from django.template.loader import render_to_string
 from django.core.urlresolvers import reverse
 
 from spirit.comment.models import Comment
+from spirit.comment.bookmark.models import CommentBookmark
 from spirit.topic.notification.models import TopicNotification
 from huey.djhuey import task
 
 
 @task()
 @receiver(post_save, sender=Comment)
+@receiver(post_save, sender=CommentBookmark)
 def send_email_notification(sender, instance, raw, **kwargs):
-    quote_url = reverse(
-        'spirit:comment:publish',
-        kwargs={'topic_id': instance.topic.id, 'pk': instance.id})
-    body_context = {
-        'comment': instance,
-        'quote_url': quote_url,
-        'domain': settings.DOMAIN
-    }
-    body = render_to_string('spirit_email_notification/new_comment.html', body_context)
-    comment_all = Comment.objects.filter(topic=instance.topic).exclude(user=instance.user)
-    topic_notification_all = TopicNotification.objects.filter(topic=instance.topic).exclude(is_active=True)
-    email_not_send = {topic.user.email for topic in topic_notification_all}
-    email_to_send = {comment.user.email for comment in comment_all}
-    email_to_send.difference_update(email_not_send)
-    for email in email_to_send:
-        send_mail(
-            'EoC Comment. {}'.format(instance.topic.title),
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [email])
+    if isinstance(instance, Comment):
+        quote_url = reverse(
+            'spirit:comment:publish',
+            kwargs={'topic_id': instance.topic.id, 'pk': instance.id})
+        body_context = {
+            'comment': instance,
+            'quote_url': quote_url,
+            'domain': settings.DOMAIN
+        }
+        body = render_to_string('spirit_email_notification/new_comment.html', body_context)
+        comment_all = Comment.objects.filter(topic=instance.topic).exclude(user=instance.user)
+        topic_notification_all = TopicNotification.objects.filter(topic=instance.topic).exclude(is_active=True)
+        email_not_send = {topic.user.email for topic in topic_notification_all}
+        email_to_send = {comment.user.email for comment in comment_all}
+        email_to_send.difference_update(email_not_send)
+        for email in email_to_send:
+            send_mail(
+                'EoC Comment. {}'.format(instance.topic.title),
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [email])
+    else:
+        comment_all = Comment.objects.filter(topic=instance.topic)
+        for comment in comment_all:
+            quote_url = reverse(
+                'spirit:comment:publish',
+                kwargs={'topic_id': instance.topic.id, 'pk': comment.id})
+            body_context = {
+                'comment': comment,
+                'quote_url': quote_url,
+                'domain': settings.DOMAIN
+            }
+        body = render_to_string('spirit_email_notification/mention.html', body_context)
+        topic_notification_all = TopicNotification.objects.filter(topic=instance.topic, action=1).exclude(user=instance.user)
+        email_to_send = {topic.user.email for topic in topic_notification_all}
+        comment_all = Comment.objects.filter(topic=instance.topic).exclude(user=instance.user)
+        email_not_send = {comment.user.email for comment in comment_all}
+        email_to_send.difference_update(email_not_send)
+        for email in email_to_send:
+            send_mail('EoC Mention. {}'.format(instance.topic.title),
+                      body,
+                      settings.DEFAULT_FROM_EMAIL,
+                      [email])
+
+
 
 
 def send_report_email(sender, instance, raw, **kwargs):
